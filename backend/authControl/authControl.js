1fe667fb9782e577098d6069da847c41bab644a9
Alex Bulganin <90492542+doal116@users.noreply.github.com>
2023-03-20 10:43:32 +0200

    fix: moving all backend processes into backend folder

---
+++ backend/authControl/authControl.js
@@ -0,0 +1,46 @@
+import User from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+
+const tokenCreation = (id) => jwt.sign({ id }, process.env.JWTTOKENSECRET, { expiresIn: 3 * 24 * 60 * 60 });
+
+
+const signupGet = (req, res) => {
+    res.send('<h1>signup</h1>');
+}
+const loginGet = (req, res) => {
+    res.send('<h1>login</h1>');
+}
+const signupPost = async (req, res) => {
+
+    const { email, password } = req.body;
+    const userCreation = await User.signUp(email, password);
+
+    if (userCreation['errors']) res.status(404).json(userCreation['errors']);
+    else {
+        const token = tokenCreation(userCreation._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: 3 * 24 * 60 * 60 * 1000 })
+            .status(200)
+            .json(userCreation._id);
+    }
+
+}
+const loginPost = async (req, res) => {
+    const { email, password } = req.body;
+    const userLogin = await User.login(email, password);
+    if (userLogin['errors']) {
+        res.status(404).json(userLogin['errors']);
+    } else {
+        const token = tokenCreation(userCreation._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: 3 * 24 * 60 * 60 * 1000 })
+            .status(200)
+            .json(userLogin._id);
+    }
+}
+
+export {
+    signupGet, loginGet,
+    signupPost, loginPost
+}
